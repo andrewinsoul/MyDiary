24787ecf2acc709d0293b151a2566a4cbc63e048
andrewinsoul <andrewinsoul@gmail.com>
2018-07-11 12:48:44 -0700

    populate entry.js file with dummy data

---
+++ server/models/entry.js
@@ -0,0 +1,12 @@
+const entries = [
+  {
+    id: 1, entry: 'My pre-andela days made me realize that you must finish a project before starting', diaryId: 1,
+  },
+  {
+    id: 2, entry: 'Hustling without favour is futile and favour without hustling is gold cast before swines. My point is that you need both', diaryId: 3,
+  },
+  {
+    id: 3, entry: 'My music album believe was born out of hard times in Abuja.', diaryId: 2,
+  },
+];
+export default entries;
