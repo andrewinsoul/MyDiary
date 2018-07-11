8646dd90d76969e568c4e5c6c4dcbf4a77674eda
andrewinsoul <andrewinsoul@gmail.com>
2018-07-11 12:47:13 -0700

    populate diary.js file with dummy-data

---
+++ server/models/diary.js
@@ -0,0 +1,12 @@
+const diaries = [
+  {
+    id: 1, name: 'Pre-Andela Days', description: 'My day-to-day activities to get into Andela', type: 'public', userId: 1,
+  },
+  {
+    id: 2, name: 'Abuja Days', description: 'Source of Inspiration to my Music Album', type: 'private', userId: 2,
+  },
+  {
+    id: 3, name: 'Grass2Glory', description: 'My Journey from the streets to palace of luxury', type: 'public', userId: 3,
+  },
+];
+export default diaries;
