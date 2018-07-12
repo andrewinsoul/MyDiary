c60fb9123b7fc7b98661fd1d91fd8436318d96a4
andrewinsoul <andrewinsoul@gmail.com>
2018-07-12 05:39:17 -0700

    add login route for input validation

--- server/middlewares/validateFunction.js
+++ server/middlewares/validateFunction.js
@@ -7,6 +7,10 @@ export default function validateUserResource(req, res, next) {
       req.checkBody('password1', 'password1 is required').notEmpty();
       req.checkBody('password2', 'password2 is required').notEmpty();
     }
+    else if (req.url === '/login') {
+      req.checkBody('email', 'a valid mail is required').notEmpty().isEmail();
+      req.checkBody('password', 'password is required').notEmpty();
+    }
   }
   const error = req.validationErrors();
   if (error) return res.status(400).send({ message: 'validation failed', failures: error });
