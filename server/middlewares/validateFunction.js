ab4c176b3ea232ebe074acec7e7f5b22f878aced
andrewinsoul <andrewinsoul@gmail.com>
2018-07-12 04:34:03 -0700

    add file which validates user input with express-validator

---
+++ server/middlewares/validateFunction.js
@@ -0,0 +1,14 @@
+export default function validateUserResource(req, res, next) {
+  if (req.method === 'POST') {
+    if (req.url === '/users') {
+      req.checkBody('name', 'name isi required').notEmpty();
+      req.checkBody('email', 'a valid mail is required').notEmpty().isEmail();
+      req.checkBody('username', 'username is required').notEmpty();
+      req.checkBody('password1', 'password1 is required').notEmpty();
+      req.checkBody('password2', 'password2 is required').notEmpty();
+    }
+  }
+  const error = req.validationErrors();
+  if (error) return res.status(400).send({ message: 'validation failed', failures: error });
+  return next();
+}
