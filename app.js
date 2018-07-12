c4c6f93872a75847bcf9ef3a6c94c1dd3c4f20d0
andrewinsoul <andrewinsoul@gmail.com>
2018-07-12 04:42:25 -0700

    import userRouter into entry file

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import ExpressValidator from 'express-validator';
+import userRouter from './server/routes/user';
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ExpressValidator());
+app.use('/api/v1', userRouter);
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 
-app.listen(() => console.log(`server live on port ${port}`));
+app.listen(port, () => console.log(`server live on port ${port}`));
