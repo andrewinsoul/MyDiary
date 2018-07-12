a3e98df8e419411bf0f438270077dae8571cb5d9
andrewinsoul <andrewinsoul@gmail.com>
2018-07-12 04:28:50 -0700

    disable linebreak-style and import express-validator middleware

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
         "no-console": 0,
+        "linebreak-style": 0,
     }
 };
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import ExpressValidator from 'express-validator';
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ExpressValidator());
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 
