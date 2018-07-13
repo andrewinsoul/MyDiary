b82aa456caa05c28415c454ae748a598cb36cbf8
andrewinsoul <andrewinsoul@gmail.com>
2018-07-13 16:22:54 -0700

    test method that updates an entry

--- server/test/c-diary-entry.js
+++ server/test/c-diary-entry.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../app';
+import entries from '../models/entry';
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -45,6 +46,23 @@ describe('MyDiary dummy-data backend tests for entry model', () => {
           expect(res).to.have.status(200);
           expect(res.body).to.have.property('msg');
           expect(res.body.msg.length).to.eql(4);
+          done();
+        });
+    });
+  });
+
+  describe('tests for method that updates an existing entry', () => {
+    it('should return code 200 message entry successfully updated', (done) => {
+      chai.request(app)
+        .put('/api/v1/entry/1')
+        .send({
+          entry: 'why now',
+        })
+        .end((err, res) => {
+          console.log(entries[0].entry === 'why now');
+          expect(res).to.have.status(200);
+          expect(res.body.msg).to.eql('entry successfully modified');
+          expect(entries[0].entry).to.eql('why now');
           done();
         });
     });
