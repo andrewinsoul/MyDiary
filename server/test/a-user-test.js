ef9ea16d0db8694bcfc55804dfb1267a493b2a71
andrewinsoul <andrewinsoul@gmail.com>
2018-07-13 11:56:21 -0700

    add test for method that adds a user

---
+++ server/test/a-user-test.js
@@ -0,0 +1,35 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+const server = chai.request(app);
+
+describe('MyDiary dummy-data backend tests for user model', () => {
+  describe('tests for method that adds a user', () => {
+    it('should return code 201 with object of user just added', (done) => {
+      server
+        .post('/api/v1/users')
+        .send({
+          name: 'Tracey Sophia',
+          username: 'sophee',
+          email: 'sophee23@yahoo.com',
+          password1: 'sophee23',
+          password2: 'sophee23',
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(201);
+          expect(res.body).to.have.property('msg');
+          expect(res.body.msg).to.eql({
+            id: 4,
+            name: 'Tracey Sophia',
+            username: 'sophee',
+            email: 'sophee23@yahoo.com',
+            password: 'sophee23',
+          });
+          done();
+        });
+    });
+  });
+});
