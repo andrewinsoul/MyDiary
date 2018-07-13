2984cc2a89d5efb110f1bbf137b34f1a147bfc8c
andrewinsoul <andrewinsoul@gmail.com>
2018-07-13 04:45:53 -0700

    add middleware that checks for diary existence

--- server/routes/entry.js
+++ server/routes/entry.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import entryHandler from '../controllers/entryController';
 import validateUserInput from '../middlewares/validateFunction';
+import appMiddleware from '../middlewares/helperClass';
 
 const entryRouter = express.Router();
 
-entryRouter.post('/entries', [validateUserInput, entryHandler.createEntry]);
+entryRouter.post('/entries', [validateUserInput, appMiddleware.checkDiaryIndex, entryHandler.createEntry]);
 
 export default entryRouter;
