7defc4476fed7b16b2eb32df73e9e6b63fb637d2
andrewinsoul <andrewinsoul@gmail.com>
2018-07-12 20:39:16 -0700

    add method that deletes a diary

--- server/controllers/diaryController.js
+++ server/controllers/diaryController.js
@@ -13,4 +13,13 @@ export default class diaryHandler {
     diary.splice(diary.length - 2, 1);
     return res.status(200).send({ msg: newDiary });
   }
+
+  static deleteDiary(req, res) {
+    const diaryIndex = diary.findIndex(item => item.id === Number(req.params.id));
+    if (diaryIndex === -1) {
+      return res.status(404).send({ error: 'Diary not found' });
+    }
+    diary.splice(diaryIndex, 1);
+    return res.status(204).send({ msg: 'diary successfully deleted' });
+  }
 }
