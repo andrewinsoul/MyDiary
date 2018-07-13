17b68b846bb76c440d73d5a20846bd14356ea242
andrewinsoul <andrewinsoul@gmail.com>
2018-07-12 19:54:41 -0700

    seperate validations for each condition

--- server/middlewares/validateFunction.js
+++ server/middlewares/validateFunction.js
@@ -2,18 +2,21 @@ export default function validateUserResource(req, res, next) {
   if (req.method === 'POST') {
     if (req.url === '/users') {
       req.checkBody('name', 'name isi required').notEmpty();
-      req.checkBody('email', 'a valid mail is required').notEmpty().isEmail();
+      req.checkBody('email', 'email is required').notEmpty();
+      req.checkBody('email', 'email should be valid').isEmail();
       req.checkBody('username', 'username is required').notEmpty();
       req.checkBody('password1', 'password1 is required').notEmpty();
       req.checkBody('password2', 'password2 is required').notEmpty();
     }
     else if (req.url === '/login') {
-      req.checkBody('email', 'a valid mail is required').notEmpty().isEmail();
+      req.checkBody('email', 'email is required').notEmpty();
+      req.checkBody('email', 'email should be valid').isEmail();
       req.checkBody('password', 'password is required').notEmpty();
     }
     else if (req.url === '/diaries') {
       req.checkBody('name', 'name is required').notEmpty();
-      req.checkBody('userId', 'an integer value of userId is required').notEmpty().isInt();
+      req.checkBody('userId', 'userId is required').notEmpty();
+      req.checkBody('userId', 'userId should be an integer').isInt();
       req.checkBody('desc', 'description is required').notEmpty();
       req.checkBody('desc', 'the description field must not be greater than 150 characters').isLength({ max: 150 });
       req.checkBody('type', 'type is required').notEmpty();
