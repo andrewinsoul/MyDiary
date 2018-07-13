8272401ddeb753abdb31079e188076f531df7af6
andrewinsoul <andrewinsoul@gmail.com>
2018-07-12 20:40:25 -0700

    add route to delete diary method

--- server/routes/diary.js
+++ server/routes/diary.js
@@ -4,6 +4,8 @@ import validateUserInput from '../middlewares/validateFunction';
 import appMiddleware from '../middlewares/helperClass';
 
 const diaryRouter = express.Router();
-diaryRouter.post('/diaries', [validateUserInput, appMiddleware.checkUserIndex, appMiddleware.addDiaryMiddleware, diaryHandler.createDiary]);
+diaryRouter
+  .post('/diaries', [validateUserInput, appMiddleware.checkUserIndex, appMiddleware.addDiaryMiddleware, diaryHandler.createDiary])
+  .delete('/diaries/:id', diaryHandler.deleteDiary);
 
 export default diaryRouter;
