0d9e601ae501ca8f95af86485356090cb2afda82
andrewinsoul <andrewinsoul@gmail.com>
2018-07-13 14:25:06 -0700

    delete server variable because it was preventing test from running

--- server/test/a-user-test.js
+++ server/test/a-user-test.js
@@ -4,12 +4,11 @@ import app from '../../app';
 
 chai.use(chaiHttp);
 const { expect } = chai;
-const server = chai.request(app);
 
 describe('MyDiary dummy-data backend tests for user model', () => {
   describe('tests for method that adds a user', () => {
     it('should return code 201 with object of user just added', (done) => {
-      server
+      chai.request(app)
         .post('/api/v1/users')
         .send({
           name: 'Tracey Sophia',
@@ -28,6 +27,23 @@ describe('MyDiary dummy-data backend tests for user model', () => {
             email: 'sophee23@yahoo.com',
             password: 'sophee23',
           });
+          done();
+        });
+    });
+  });
+
+  describe('tests for method that logins a user', () => {
+    it('should return code 200 with message user logged in', (done) => {
+      chai.request(app)
+        .post('/api/v1/login')
+        .send({
+          email: 'andrewinsoul@gmail.com',
+          password: 'andyjs+py',
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.property('msg');
+          expect(res.body.msg).to.eql('signed in as andyjs');
           done();
         });
     });
