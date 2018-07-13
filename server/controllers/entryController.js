bf383c46e6511ad3f0596efcc9490198c0b972d3
andrewinsoul <andrewinsoul@gmail.com>
2018-07-13 06:50:25 -0700

    fix a bug

--- server/controllers/entryController.js
+++ server/controllers/entryController.js
@@ -26,7 +26,7 @@ export default class entryHandler {
 
   static modifyEntry(req, res) {
     const index = entries.findIndex(entry => entry.id === Number(req.params.id));
-    entries[index].profile = req.body.profile;
+    entries[index].entry = req.body.entry;
     return res.status(200).send({ msg: 'entry successfully modified' });
   }
 }
