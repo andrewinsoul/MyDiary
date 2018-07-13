6c4d1fa5cdc441eaa4f3bebda9328d36dd0cb2ce
andrewinsoul <andrewinsoul@gmail.com>
2018-07-12 19:15:49 -0700

    add input validation for create diary route

--- server/middlewares/validateFunction.js
+++ server/middlewares/validateFunction.js
@@ -11,6 +11,13 @@ export default function validateUserResource(req, res, next) {
       req.checkBody('email', 'a valid mail is required').notEmpty().isEmail();
       req.checkBody('password', 'password is required').notEmpty();
     }
+    else if (req.url === '/diaries') {
+      req.checkBody('name', 'name is required').notEmpty();
+      req.checkBody('userId', 'an integer value of userId is required').notEmpty().isInt();
+      req.checkBody('desc', 'description is required').notEmpty();
+      req.checkBody('desc', 'the description field must not be greater than 150 characters').isLength({ max: 150 });
+      req.checkBody('type', 'type is required').notEmpty();
+    }
   }
   const error = req.validationErrors();
   if (error) return res.status(400).send({ message: 'validation failed', failures: error });
