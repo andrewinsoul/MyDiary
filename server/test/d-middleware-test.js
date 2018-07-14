a0ed32c105feff7c60329e293f9527ea0625d03e
andrewinsoul <andrewinsoul@gmail.com>
2018-07-13 19:01:10 -0700

    test middleware that checks username conflict

--- server/test/d-middleware-test.js
+++ server/test/d-middleware-test.js
@@ -1,7 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../app';
-import validateUserInput from '../middlewares/validateFunction';
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -81,6 +80,66 @@ describe('MyDiary dummy-data backend tests for middleware validating user input'
           expect(res).to.have.status(400);
           expect(res.body).to.have.property('failures');
           expect(res.body.message).to.eql('validation failed');
+          done();
+        });
+    });
+  });
+
+  describe('test for middleware that checks for certain conditions', () => {
+    const userBadPassword = {
+      email: 'azukaokoye99@yahoo.com',
+      username: 'az',
+      name: 'Veronica',
+      password1: 'qwerty',
+      password2: 'asdfgh',
+    };
+    const userBadMail = {
+      email: 'andrewinsoul@gmail.com',
+      username: 'aaaa',
+      name: 'Andrew Johnson',
+      password1: 'qwertyuiop',
+      password2: 'qwertyuiop',
+    };
+
+    const userBadUsername = {
+      email: 'azukaokoye99@gmail.com',
+      username: 'slava',
+      name: 'Andrew Johnson',
+      password1: 'qwertyuiop',
+      password2: 'qwertyuiop',
+    };
+    it('should return status code 409 with error message', (done) => {
+      chai.request(app)
+        .post('/api/v1/users')
+        .send(userBadPassword)
+        .end((err, res) => {
+          expect(res).to.have.status(409);
+          expect(res.body).to.have.property('error');
+          expect(res.body.error).to.eql('password mismatch');
+          done();
+        });
+    });
+
+    it('should return status code 409 with error message', (done) => {
+      chai.request(app)
+        .post('/api/v1/users')
+        .send(userBadMail)
+        .end((err, res) => {
+          expect(res).to.have.status(409);
+          expect(res.body).to.have.property('error');
+          expect(res.body.error).to.eql('user with this mail already has an account');
+          done();
+        });
+    });
+
+    it('should return status code 409 with error message', (done) => {
+      chai.request(app)
+        .post('/api/v1/users')
+        .send(userBadUsername)
+        .end((err, res) => {
+          expect(res).to.have.status(409);
+          expect(res.body).to.have.property('error');
+          expect(res.body.error).to.eql('username already taken, choose another one');
           done();
         });
     });
