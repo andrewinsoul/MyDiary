253cf7419606040a46f99d3b468c680ab9432ce4
andrewinsoul <andrewinsoul@gmail.com>
2018-07-13 22:45:27 -0700

    rename an object item for uniformity reason

--- server/models/diary.js
+++ server/models/diary.js
@@ -1,12 +1,12 @@
 const diaries = [
   {
-    id: 1, name: 'Pre-Andela Days', description: 'My day-to-day activities to get into Andela', type: 'public', userId: 1,
+    id: 1, name: 'Pre-Andela Days', desc: 'My day-to-day activities to get into Andela', type: 'public', userId: 1,
   },
   {
-    id: 2, name: 'Abuja Days', description: 'Source of Inspiration to my Music Album', type: 'private', userId: 2,
+    id: 2, name: 'Abuja Days', desc: 'Source of Inspiration to my Music Album', type: 'private', userId: 2,
   },
   {
-    id: 3, name: 'Grass2Glory', description: 'My Journey from the streets to palace of luxury', type: 'public', userId: 3,
+    id: 3, name: 'Grass2Glory', desc: 'My Journey from the streets to palace of luxury', type: 'public', userId: 3,
   },
 ];
 export default diaries;
