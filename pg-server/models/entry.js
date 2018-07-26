19cf7a0b1a2133dda9c023915cfaeb918a15c65a
andrewinsoul <andrewinsoul@gmail.com>
2018-07-25 20:16:06 -0700

    add time column in entry model

--- pg-server/models/entry.js
+++ pg-server/models/entry.js
@@ -7,7 +7,7 @@ client.connect()
   .then(() => console.log("database successfully connected"))
   .catch(() => console.log({ error: 'an error occured when trying to connect to database' }));
 const ENUMTYPE = "CREATE TYPE entryType AS ENUM ('todo', 'history')";
-const SQLCODE = "CREATE TABLE entries(entryId SERIAL PRIMARY KEY, Title VARCHAR(40) not null, Entry TEXT not null, Time TIMESTAMPTZ NOT NULL DEFAULT NOW(), type entryType, diaryId INT, FOREIGN KEY (diaryId) REFERENCES diaries (diaryId))";
+const SQLCODE = "CREATE TABLE entries(entryId SERIAL PRIMARY KEY, Title VARCHAR(40) not null, Entry TEXT not null, Time created_at TIMESTAMPTZ NOT NULL DEFAULT NOW() type entryType, diaryId INT, FOREIGN KEY (diaryId) REFERENCES diaries (diaryId))";
 let query = client.query(ENUMTYPE);
 query
   .then(() => {
