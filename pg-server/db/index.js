d99a644ef7e10fb6d7e3c459ca4ae4720c105679
andrewinsoul <andrewinsoul@gmail.com>
2018-07-26 05:15:25 -0700

    make file changes due to linting message

--- pg-server/db/index.js
+++ pg-server/db/index.js
@@ -12,9 +12,9 @@ const queryObject = {
     const start = Date.now();
     return pool.query(text, params, (err, res) => {
       const duration = Date.now() - start;
-      console.log('executed query', { text, duration, rows: res.rowCount })
-      callback(err, res)
-    })
-  }
+      console.log('executed query', { text, duration, rows: res.rowCount });
+      callback(err, res);
+    });
+  },
 };
 export default queryObject;
