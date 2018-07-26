757fd7ae135e10faa4ce9781ddce8fd8e82c064b
andrewinsoul <andrewinsoul@gmail.com>
2018-07-26 05:09:29 -0700

    add file that holds connection details

---
+++ pg-server/db/index.js
@@ -0,0 +1,20 @@
+import { Pool } from 'pg';
+
+export const pool = new Pool({
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'MyDiary',
+  password: 'postgres',
+  port: 5432,
+});
+const queryObject = {
+  query: (text, params, callback) => {
+    const start = Date.now();
+    return pool.query(text, params, (err, res) => {
+      const duration = Date.now() - start;
+      console.log('executed query', { text, duration, rows: res.rowCount })
+      callback(err, res)
+    })
+  }
+};
+export default queryObject;
