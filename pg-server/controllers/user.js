d22cabd6e84a6fb3a30c30d6e8b4525dc872d7aa
andrewinsoul <andrewinsoul@gmail.com>
2018-07-28 05:08:57 -0700

    add functionality forsigning up

---
+++ pg-server/controllers/user.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
+import client from '../config/config';
+
+dotenv.load();
+
+const userHandler = {
+  createUser(req, res) {
+    const encodedPassword = bcrypt.hashSync(req.body.password, 8);
+    const userInfo = {
+      name: req.body.name,
+      email: req.body.email,
+      password: encodedPassword,
+      username: req.body.username,
+    };
+    client.query(
+      'INSERT INTO users(Name, Username, Password, Email) values($1, $2, $3, $4)', [userInfo.name, userInfo.username, userInfo.password, userInfo.email],
+    ).then(
+      () => {
+        const token = jwt.sign(
+          {
+            id: userInfo.email,
+          },
+          process.env.jwtKEY,
+          {
+            expiresIn: 86400,
+          },
+        );
+        return res.status(201).send({ auth: true, token });
+      },
+    )
+      .catch(err => console.log(err));
+  },
+};
+export default userHandler;
