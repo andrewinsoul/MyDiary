cc20c682635e114b556ee0d1417fa8da12d039ab
andrewinsoul <andrewinsoul@gmail.com>
2018-07-28 05:29:35 -0700

    refactor to reduce import load

---
+++ pg-server/middlewares/validateUserResource.js
@@ -0,0 +1,15 @@
+export default function validateUserResource(req, res, next) {
+  if (req.method === 'POST') {
+    if (req.url === '/auth/signup') {
+      req.checkBody('name', 'name is required').notEmpty();
+      req.checkBody('email', 'email is required').notEmpty();
+      req.checkBody('email', 'email should be valid').isEmail();
+      req.checkBody('username', 'username is required').notEmpty();
+      req.checkBody('password', 'password is required').notEmpty();
+      req.checkBody('password', 'password should be at least 8 characters long').isLength({ min: 8 });
+    }
+    const error = req.validationErrors();
+    if (error) return res.status(400).send({ message: 'validation failed', failures: error });
+    return next();
+  }
+}
---
+++ pg-server/routes/userRouter.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import userHandler from '../controllers/user';
+import validateUserResource from '../middlewares/validateUserResource';
+import appMiddleware from '../middlewares/appMiddleware';
+
+const userRouter = express.Router();
+
+userRouter
+  .post('/auth/signup', [validateUserResource, appMiddleware.addUserMiddleware, userHandler.createUser]);
+export default userRouter;
