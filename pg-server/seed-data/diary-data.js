0b59b717ed5085fdf4b49bc66f914db393e4beb9
andrewinsoul <andrewinsoul@gmail.com>
2018-07-29 16:51:51 -0700

    add file containing seed data that will be used in testing

---
+++ pg-server/seed-data/diary-data.js
@@ -0,0 +1,10 @@
+const diaryData = {
+  diaryWithCompleteDetails: {
+    name: 'Andrew Childhood',
+    desc: 'My childhood experiences and memories',
+    type: 'public',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFuZHJld2luc291bEBnbWFpbC5jb20iLCJpYXQiOjE1MzI5MDE3MzUsImV4cCI6MTUzMjk4ODEzNX0._-HdGzIVGUvmMjXCnE6P3oqKSea25xVZkD7-yqYXXqE',
+    userId: 1,
+  },
+};
+export default diaryData;
---
+++ pg-server/seed-data/user-data.js
@@ -0,0 +1,16 @@
+const userData = {
+  userWithCompleteDetails: {
+    name: 'Andrew Okoye',
+    password: 'pythonismystack',
+    confirmPassword: 'pythonismystack',
+    username: 'andy',
+    email: 'andrewinsoul@gmail.com',
+  },
+  userWithIncompleteDetails: {
+    name: 'Andrew Okoye',
+    password: 'pythonismystack',
+    confirmPassword: 'pythonismystack',
+    username: 'andy',
+  },
+};
+export default userData;
