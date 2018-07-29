cc5a4aefc6682849c0e2296a2c11ba755002f75c
andrewinsoul <andrewinsoul@gmail.com>
2018-07-28 17:53:55 -0700

    remove enum type in diary table

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ import ExpressValidator from 'express-validator';
 import swaggerUi from 'swagger-ui-express';
 import sqlCode from './pg-server/sql/sqlObject';
 import userRoute from './pg-server/routes/userRouter';
+import diaryRoute from './pg-server/routes/diaryRouter';
 import client from './pg-server/config/config';
 
 let query = client.query(sqlCode.CreateUserTable);
 query.then(() => console.log('users table successfully created'));
-query = client.query(sqlCode.DropDiaryType).then(() => console.log('type dropped'));
-query = client.query(sqlCode.CreateDiaryType);
-query.then(() => console.log('enum type created without problem'));
 query = client.query(sqlCode.CreateDiaryTable);
 query.then(() => console.log('diary table created successfully'));
 query = client.query(sqlCode.CreateEntryTable);
@@ -27,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ExpressValidator());
 app.use('/api/v1/', userRoute);
+app.use('/api/v1/', diaryRoute);
 app.get('/', (req, res) => res.status(200).send({ message: 'welcome to MyDiary API' }));
 app.get('*', (req, res) => res.redirect(302, '/api/v1/api-docs'));
 
