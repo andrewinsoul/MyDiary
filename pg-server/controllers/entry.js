e0293c40b3d5d2f38c8833c48bb66537fd03b618
andrewinsoul <andrewinsoul@gmail.com>
2018-07-28 19:48:34 -0700

    add functionality for creating an entry

---
+++ pg-server/controllers/entry.js
@@ -0,0 +1,18 @@
+import client from '../config/config';
+
+const entryHandler = {
+  addEntry(req, res) {
+    const entryInfo = {
+      title: req.body.title,
+      entry: req.body.entry,
+      diaryId: req.body.diaryId,
+    };
+    client.query(
+      'INSERT INTO entries(Title, Entry, diaryId) values($1, $2, $3) RETURNING *', [entryInfo.title, entryInfo.entry, entryInfo.diaryId],
+    ).then(
+      result => res.status(201).send({ message: result.rows }),
+    )
+      .catch(error => res.status(500).send({ error }));
+  },
+};
+export default entryHandler;
