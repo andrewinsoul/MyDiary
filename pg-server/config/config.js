8980db61262ed4420debeeb1358013ed8eb61660
andrewinsoul <andrewinsoul@gmail.com>
2018-08-01 02:39:47 -0700

    add logic for choosing connection details when on heroku

--- pg-server/config/config.js
+++ pg-server/config/config.js
@@ -2,8 +2,13 @@ import pg from 'pg';
 import connString from '../db/index';
 
 const env = process.env.NODE_ENV || 'development';
-
-const client = new pg.Client(connString[env]);
+let client;
+if (process.env.NODE_ENV === 'production') {
+  client = new pg.Client(connString.production.DATABASE_URL);
+}
+else {
+  client = new pg.Client(connString[env]);
+}
 client.connect()
   .then(() => console.log('database successfully connected'))
   .catch(error => console.log({ error }));
