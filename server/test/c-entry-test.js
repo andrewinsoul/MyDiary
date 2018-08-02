6db501a5f228c37f8d20e5e7fe7d0d4955a37d80
andrewinsoul <andrewinsoul@gmail.com>
2018-08-01 21:59:45 -0700

    increase coverage

--- server/test/c-entry-test.js
+++ server/test/c-entry-test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../app';
+import entryData from '../seed-data/entry-data';
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -24,10 +25,41 @@ describe('MyDiary backend tests with postgres database for entry model', () => {
     });
   });
 
+  describe('tests method that adds an entry in the databse', () => {
+    it('should return code 201 with entry just added', (done) => {
+      chai.request(app)
+        .post('/api/v1/entries')
+        .send(entryData.entryWithCompleteDetails)
+        .set('x-access-token', tokenValue)
+        .end((err, res) => {
+          expect(res).to.have.status(201);
+          expect(res.body).to.have.property('message');
+          done();
+        });
+    });
+  });
+
   describe('tests method that gets all entry in the databse', () => {
     it('should return code 200 with array containing all entries', (done) => {
       chai.request(app)
         .get('/api/v1/entries')
+        .set('x-access-token', tokenValue)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.property('message');
+          done();
+        });
+    });
+  });
+
+  describe('tests method that modifies an entry in the databse', () => {
+    it('should return code 200 with array containing modified entry', (done) => {
+      chai.request(app)
+        .put('/api/v1/entries/1')
+        .send({
+          title: 'My Best Journey',
+          entry: 'I have been through many journrys but this',
+        })
         .set('x-access-token', tokenValue)
         .end((err, res) => {
           expect(res).to.have.status(200);
