80114bb1997fe9f4e045fe9116914d4e659c6215
andrewinsoul <andrewinsoul@gmail.com>
2018-08-20 16:44:26 -0700

    modify template and make pages responsive

--- UI/assets/scripts/main.js
+++ UI/assets/scripts/main.js
@@ -2,14 +2,15 @@ const modal = document.getElementById('page-modal');
 const btn = document.getElementById('add-button');
 const mainDiv = document.getElementById('main-div');
 const closeBtn = this.document.getElementById('close');
-btn.onclick = function () {
-  modal.style.display = "block";
-  mainDiv.style.backgroundColor = "rgba(210, 200, 200, 0.6)";
-}
+// btn.onclick = function () {
+//   modal.style.display = "block";
+//   mainDiv.style.backgroundColor = "rgba(210, 200, 200, 0.6)";
+// }
 function openLink(theUrl) {
   document.location.href = theUrl;
 }
-closeBtn.onclick = function () {
-  modal.style.display = "none";
-  mainDiv.style.backgroundColor = "#266189"
-}
+
+// closeBtn.onclick = function () {
+//   modal.style.display = "none";
+//   mainDiv.style.backgroundColor = "#266189"
+// }
