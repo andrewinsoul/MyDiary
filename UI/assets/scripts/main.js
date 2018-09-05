ce782eac04b95ee06f8302ecf9b0fe1a7b07ee30
andrewinsoul <andrewinsoul@gmail.com>
2018-09-04 18:01:12 -0700

    add edit entry feature

--- UI/assets/scripts/main.js
+++ UI/assets/scripts/main.js
@@ -1,16 +1,6 @@
-const modal = document.getElementById('page-modal');
 const btn = document.getElementById('add-button');
 const mainDiv = document.getElementById('main-div');
 const closeBtn = this.document.getElementById('close');
-// btn.onclick = function () {
-//   modal.style.display = "block";
-//   mainDiv.style.backgroundColor = "rgba(210, 200, 200, 0.6)";
-// }
 function openLink(theUrl) {
   document.location.href = theUrl;
 }
-
-// closeBtn.onclick = function () {
-//   modal.style.display = "none";
-//   mainDiv.style.backgroundColor = "#266189"
-// }
