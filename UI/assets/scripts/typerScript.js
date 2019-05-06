5933d9bb7e53057fadc1e439dbdd29b8292fd485
andrewinsoul <andrewinsoul@gmail.com>
2019-05-06 09:36:53 +0100

    add homepage template

---
+++ UI/assets/scripts/typerScript.js
@@ -0,0 +1,10 @@
+
+let options = {
+    strings: ["Welcome to MyDiary","Helping you keep log of your Memorable Experiences","Create an account and manage your own diary"],
+    typeSpeed: 22,
+    loop: true,
+    backSpeed: 55,
+    showCursor: false
+  }
+  
+  let typed = new Typed(".cover-heading", options)
